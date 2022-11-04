d1b920a28f72de47e2143ead824b909223f24e42
ivan <ivan_ruiz2301@hotmail.com>
2022-11-04 18:27:44 -0300

    login tarea

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('dotenv').config();
+var session = require ('express-session');
 
 var indexRouter = require('./routes/index');
 
@@ -13,6 +14,8 @@ var instalacionesrouter = require('./routes/instalaciones');
 var galeriarouter = require('./routes/galeria');
 var novedadesrouter = require('./routes/novedades');
 var contactorouter = require('./routes/contacto');
+var loginrouter = require('./routes/admin/login');
+var adminRouter = require('./routes/admin/novedades');
 
 var app = express();
 
@@ -26,6 +29,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session ({
+  secret: 'mauqpnht213trq',
+  resave: false , 
+  saveUninitialized:true
+}))
+secured = async (req,res,next) => {
+  try{
+    console.log(req.session.id_usuario);
+    if (req.session.id_usuario) {
+      next();
+    } else {res.redirect('/admin/login');
+  }
+} catch (error) {
+  console.log (error);
+}
+}
+
+
 app.use('/', indexRouter);
 
 app.use('/docentes', docentesrouter);
@@ -33,6 +54,8 @@ app.use('/instalaciones', instalacionesrouter);
 app.use('/galeria', galeriarouter );
 app.use('/novedades', novedadesrouter);
 app.use('/contacto', contactorouter);
+app.use('/admin/login',loginrouter);
+app.use('/admin/novedades',secured,adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ models/usuariosModels.js
@@ -0,0 +1,15 @@
+var pol = require('./bd');
+var md5 = require('md5');
+const pool = require('./bd');
+
+async function getUserbyUsernameAndPassword(user, password) {
+    try {
+        var query = 'select * from usuarios where usuario = ? and password = ? limit 1';
+        var rows = await pool.query(query, [user , md5 (password)]);
+        return rows[0];
+    } catch (error) {
+        console.log(error);
+    } 
+}
+
+module.exports = {getUserbyUsernameAndPassword}
